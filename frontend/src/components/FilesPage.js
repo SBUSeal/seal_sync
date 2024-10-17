bf276a45411e6781117e11d3d3239e644a6743a9
Jacob Sinanan <jacob.sinanan@stonybrook.edu>
2024-10-17 19:22:23 -0400

    Error handling for file upload

--- frontend/src/components/FilesPage.js
+++ frontend/src/components/FilesPage.js
@@ -212,7 +212,7 @@ const FilesPage = () => {
             ></textarea>
             <div className="modal-actions">
               <button onClick={handleModalClose}>Cancel</button>
-              <button onClick={handleModalSubmit}>Submit</button>
+              <button onClick={handleModalSubmit} disabled={newFileDetails.price.trim() === '' || newFileDetails.description.trim() === ''}>Submit</button>
             </div>
           </div>
         </div>
