b3aa18c20bf326869045e9197df8b7baa70bad88
Usman Shahid <usman.shahid@stonybrook.edu>
2024-11-19 11:39:08 -0500

    MiningPage styling updated + CPU/GPU dummy data

--- frontend/src/components/MiningPage.js
+++ frontend/src/components/MiningPage.js
@@ -4,12 +4,17 @@ import '../stylesheets/MiningPage.css';
 const MiningPage = ({sealTokenBalance, setSealTokenBalance, miningLog, setMiningLog}) => {
     const [isMining, setIsMining] = useState(false);
     const [hashPower, setHashPower] = useState(492.44);
+    const [cpuUsage, setCpuUsage] = useState(0);
+    const [gpuUsage, setGpuUsage] = useState(0);
 
     useEffect(() => {
         let interval;
         if (isMining) {
             interval = setInterval(() => {
                 setSealTokenBalance(prevBalance => prevBalance + (hashPower * 0.0001));
+
+                setCpuUsage((Math.random() * 40 + 20).toFixed(2))//20% to 60%
+                setGpuUsage((Math.random() *30 + 10).toFixed(2));//10% to 40%
                 if (miningLog.length === 0 || miningLog[0].type === 'stop') {
                     const startEntry = {
                         type: 'start',
@@ -42,6 +47,8 @@ const MiningPage = ({sealTokenBalance, setSealTokenBalance, miningLog, setMining
                 <div className="tile">
                     <h2>Balance: {sealTokenBalance.toFixed(2)} SKT</h2>
                     <h2>Hash Power: {hashPower} MH/s</h2>
+                    <h2>CPU Usage: {cpuUsage}%</h2>
+                    <h2>GPU Usage: {gpuUsage}%</h2> 
                 </div>
                 <div className="mining-control">
                     <label className="switch">
