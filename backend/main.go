115b0d73625a4a9eaf948c3b19f33b8a87490249
Jacob Sinanan <jacob.sinanan@stonybrook.edu>
2024-12-10 18:52:37 -0500

    Get rid of duplicate function

--- backend/main.go
+++ backend/main.go
@@ -7,13 +7,11 @@ import (
 	"net/http"
 	"sync"
 	"time"
-	"sync"
 )
 
 var (
-	node_id         = "114418346" // give your SBU ID
-	relay_node_addr = "/ip4/130.245.173.221/tcp/4001/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN"
-	// bootstrap_node_addr = "/ip4/130.245.173.222/tcp/61000/p2p/12D3KooWQd1K1k8XA9xVEzSAu7HUCodC7LJB6uW5Kw4VwkRdstPE"
+	node_id               = "114418346" // give your SBU ID
+	relay_node_addr       = "/ip4/130.245.173.221/tcp/4001/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN"
 	bootstrap_node_1_addr = "/ip4/130.245.173.221/tcp/6001/p2p/12D3KooWE1xpVccUXZJWZLVWPxXzUJQ7kMqN8UQ2WLn9uQVytmdA"
 	bootstrap_node_2_addr = "/ip4/130.245.173.222/tcp/61020/p2p/12D3KooWM8uovScE5NPihSCKhXe8sbgdJAi88i2aXT2MmwjGWoSX"
 	globalCtx             = context.Background()
@@ -25,21 +23,7 @@ var (
 
 	miningEnabled = true
 	miningMutex   sync.Mutex
-
 )
-func enableCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins (or restrict to specific domains)
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
 
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -102,12 +86,10 @@ func main() {
 	mux.HandleFunc("/deleteUploadedFile/{cid}", deleteUploadedFile)
 	mux.HandleFunc("/deleteDownloadedFile/{cid}", deleteDownloadedFile)
 
-
 	mux.HandleFunc("/startMining", HandleStartMining)
 	mux.HandleFunc("/stopMining", HandleStopMining)
 
 	mux.HandleFunc("/getBalance", HandleGetBalance)
-
 
 	fmt.Println("Server is running on port 8080")
 	handler := enableCORS(mux)
